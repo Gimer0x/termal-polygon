ffa990fc38b0384f7749a5d304cb2760bdb77d0a
DappDojo <gimer@dappdojo.com>
2022-11-01 22:26:12 -0600

    update

--- migrations/2_deploy_termal.js
+++ migrations/2_deploy_termal.js
@@ -10,22 +10,22 @@ require('dotenv').config({path: '../.env'});
 module.exports = async function (deployer, network, accounts) {
   let addr = await web3.eth.getAccounts();
 
-  console.log("Deployer's account: ", addr[0]);
   if (network == "local"){
+    console.log("Deployer's account: ", accounts[0]);
     let daiInstance = await deployer.deploy(DaiToken, process.env.INITIAL_TOKENS, {from: addr[0]}); 
     let termalToken = await deployer.deploy(TermalToken, process.env.INITIAL_TOKENS, {from: addr[0]});
     let startupToken = await deployer.deploy(StartupToken, process.env.STARTUP_TOKENS, {from: addr[2]});
     
     let investorsHandler = await deployer.deploy(InvestorsHandler, {from: addr[0]});
     let startupsHandler = await deployer.deploy(StartupsHandler, {from: addr[0]});
-
+    //console.log("address: ", daiInstance);
     await deployer.deploy(Termal, daiInstance.address, termalToken.address, investorsHandler.address, startupsHandler.address, {from: addr[0], value: 1000000000000000000});
 
     let termal = await Termal.deployed();
     await termalToken.transfer(termal.address, process.env.TOKENS_TO_CONTRACT, {from:addr[0]});
     await daiInstance.transfer(process.env.INVESTOR_ACCOUNT_1, process.env.DAI_TO_INVESTOR, {from:addr[0]});
     await daiInstance.transfer(process.env.INVESTOR_ACCOUNT_2, process.env.DAI_TO_INVESTOR, {from:addr[0]});
-
+    
   }
   else if (network == "kovan") {
     let termalToken = await deployer.deploy(TermalToken, process.env.INITIAL_TOKENS, {from: addr[0]});
